93bd96c642cf707e8d6946e0e494e1fabc26bebd
Justin Large <justinlarge1974@gmail.com>
2020-04-24 17:55:24 -0700

    Added a temp DynamoDB table to test out remote state and locking

--- site/main.tf
+++ site/main.tf
@@ -2,7 +2,7 @@ variable "aws_access_key_id" {}
 variable "aws_secret_access_key" {}
 
 locals {
-  region = 'us-west-2'
+  main_ui_bucket = "daily-wombat-terraform-state"
 
   tags = {
     site = "dailywombat"
@@ -12,13 +12,27 @@ locals {
 provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
-  region     = var.region
+  region     = "us-west-2"
 }
 
 terraform {
   backend "s3" {
     bucket = "daily-wombat-terraform-state"
     key    = "main"
-    region = var.region
+    region = "us-west-2"
   }
+}
+
+resource "aws_dynamodb_table" "funtimes" {
+  name           = "funtimes"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = local.tags
 }
