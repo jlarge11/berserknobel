44a88ec4f2e08c068e1a13689a9d2f1cbe0ad764
Justin Large <justinlarge1974@gmail.com>
2020-05-03 18:46:31 -0700

    Broke up the dns folder

---
+++ dns/setup.tf
@@ -0,0 +1,39 @@
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+
+locals {
+  site_name = "dailywombat"
+  domain = "${local.site_name}.com"
+
+  tags = {
+    site = local.site_name
+  }
+}
+
+provider "aws" {
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  region     = "us-west-1"
+}
+
+terraform {
+  backend "remote" {
+    organization = "dailywombat"
+
+    workspaces {
+      prefix = "dns-"
+    }
+  }
+}
+
+data "terraform_remote_state" "site" {
+  backend = "remote"
+
+  config = {
+    organization = local.site_name
+
+    workspaces = {
+      name = "site-prod" # or site-${var.environment} and set environment in TF cloud
+    }
+  }
+}
